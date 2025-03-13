2b8ebfe02857cc6ffbb1ef700c4415ee44c95f8e
Christine Macharia <christineolivia7gmail.com>
2025-03-13 13:10:25 -0700

    corrected import error

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,7 @@ const User = require("../models/user");
 const { createToken } = require("../helpers/tokens");
 const userNewSchema = require("../schemas/userNew.json");
 const userUpdateSchema = require("../schemas/userUpdate.json");
+const db = require("../db");
 
 const router = express.Router();
 
